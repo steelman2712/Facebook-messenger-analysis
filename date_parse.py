ac5b194f38136418c3fec8b04c4922230c7176ce
steelman2712 <50148684+steelman2712@users.noreply.github.com>
2019-10-01 14:36:07 +0100

    Add files via upload

--- date_parse.py
+++ date_parse.py
@@ -1,6 +1,30 @@
 from datetime import datetime
 import calendar
 import csv
+import os
+import argparse
+
+
+##Set up cli options
+cmd_dir = os.getcwd() #Get CLI location
+parser = argparse.ArgumentParser(description="Converts Facebook messenger html data to a csv format")
+parser.add_argument("-i",'--infile', nargs='?',dest="infile", type=str,default=os.path.join(cmd_dir,"htmlcsv.csv"),help="Input html file to be converted, the default is in the current location of the cli and called 'htmlcsv.csv'") #Add input argument
+parser.add_argument("-o",'--outfile', nargs='?',dest="outfile", type=str,default=os.path.join(cmd_dir,"htmlcsv.csv"), help="Output csv file to write to, the default the same file as the input")	#Out output file argument
+args = parser.parse_args()
+
+
+#Check input file is valid
+try:
+	open(args.infile,"r")
+except:
+	print("Input file not found, the input file is specified to be at "+args.infile)
+
+infile = args.infile
+outfile = args.outfile
+
+
+
+
 
 def date_split(date_input):
     date_input= datetime.strptime(str(date_input), '%d %B %Y %H:%M')
@@ -9,7 +33,7 @@ def date_split(date_input):
 
 #print(date_split("6 September 2018 23:26"))
 
-with open("/home/reynolds/Documents/analysis/htmlcsv.csv","r") as csvfile:
+with open(infile,"r") as csvfile:
     htmlreader = csv.reader(csvfile, delimiter=',')
     next(htmlreader)
     new_rows_list = [['Name','Message','Date','Time','React']]
@@ -24,7 +48,7 @@ with open("/home/reynolds/Documents/analysis/htmlcsv.csv","r") as csvfile:
     csvfile.close()
 
 
-with open("/home/reynolds/Documents/analysis/htmlcsv.csv","w") as csv_out:
+with open(outfile,"w") as csv_out:
     writer = csv.writer(csv_out, delimiter=",")
     writer.writerows(new_rows_list)
     csv_out.close()
